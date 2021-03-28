da60b6f3d10ec0b647fa17b66766d69a945b9a6b
Your-Full-Name <your-email-address>
2021-03-28 14:53:55 -0700

    docs: Added documentation about CITY_DATA variable

--- bikeshare1.py
+++ bikeshare1.py
@@ -2,6 +2,7 @@ import time
 import pandas as pd
 import numpy as np
 
+# CITY_DATA creates a data file mapping associated to the city
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
